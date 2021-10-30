5d488fa38421a57309d4e0c0c57c4289bc9ddfcd
pritam3119 <41792675+pritam3119@users.noreply.github.com>
2021-10-30 11:09:32 +0530

    modified

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
-import java.util.List;
+#import java.util.List;
 
-@Component
+#@Component
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
